86f9cac6794db4a7a0d52e402301f0b6cebe76f5
Priyo123 <84970445+Priyo123@users.noreply.github.com>
2021-11-16 17:34:45 +0530

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,24 @@
+function cyclones()
+{
+    window.location="Cyclone.html";
+}
+function floods()
+{
+    window.location="Floods.html";
+}
+function bushfires()
+{
+    window.location="Bushfire.html";
+}
+function storms()
+{
+    window.location="Storms.html";
+}
+function tsunamis()
+{
+    window.location="Tsunamis.html";
+}
+function earthquakes()
+{
+    window.location="Earthquakes.html";
+}
